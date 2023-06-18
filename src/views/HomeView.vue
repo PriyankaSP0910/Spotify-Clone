115b80099770e039f9941c2f783a386726ee6be0
Priyanka SP <priyankasppayannawar@gmail.com>
2023-06-18 16:33:03 +0530

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,62 @@
+<script setup>
+    import HomeCard from '../components/HomeCard.vue';
+</script>
+
+<template>
+    <div class="p-8">
+        <button
+            type="button"
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+            Podcasts to make you smarter
+        </button>
+
+        <div class="py-1.5"></div>
+
+        <div class="flex items-center">
+            <HomeCard image="https://picsum.photos/id/30/300/300" title="Healthy Habits" subTitle="Kicktart your Morning routine with Martin Burg." />
+            <HomeCard image="https://picsum.photos/id/45/300/300" title="Bengaluru" subTitle="Explore Banglore with Satish Kumar." />
+            <HomeCard class="md:block hidden" image="https://picsum.photos/id/65/300/300" title="Happiness" subTitle="Find the secret to happiness with Linda Ray." />
+            <HomeCard class="lg:block hidden" image="https://picsum.photos/id/67/300/300" title="Nature's Beauty" subTitle="Explore Nature's beauty with Shamili Dixit." />
+            <HomeCard class="xl:block hidden" image="https://picsum.photos/id/100/300/300" title="Pollution" subTitle="Destruction of Mother Nature with Roy Kay. " />
+        </div>
+    </div>
+
+    <div class="p-8">
+        <button
+            type="button"
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+        Recommended radio
+        </button>
+
+        <div class="py-1.5"></div>
+
+        <div class="flex items-center">
+            <HomeCard image="https://picsum.photos/id/120/300/300" title="91.1 FM" subTitle="Radio City" />
+            <HomeCard image="https://picsum.photos/id/110/300/300" title="91.9 FM" subTitle="Radio Indigo" />
+            <HomeCard class="md:block hidden" image="https://picsum.photos/id/221/300/300" title="92.7 FM" subTitle="Big FM" />
+            <HomeCard class="lg:block hidden" image="https://picsum.photos/id/232/300/300" title="93.5 FM" subTitle="Red FM" />
+            <HomeCard class="xl:block hidden" image="https://picsum.photos/id/187/300/300" title="94.3 FM" subTitle="Radio One" />
+        </div>
+    </div>
+
+    <div class="p-8">
+        <button
+            type="button"
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+        Recommended songs
+        </button>
+
+        <div class="py-1.5"></div>
+
+        <div class="flex items-center">
+            <HomeCard image="https://picsum.photos/id/88/300/300" title="Solitude" subTitle="Billie Holiday" />
+            <HomeCard image="https://picsum.photos/id/243/300/300" title="Manimal" subTitle="Heskil Fent" />
+            <HomeCard class="md:block hidden" image="https://picsum.photos/id/123/300/300" title="Rings" subTitle="Ariana Grande" />
+            <HomeCard class="lg:block hidden" image="https://picsum.photos/id/121/300/300" title="Money" subTitle="Lisa Manoban" />
+            <HomeCard class="xl:block hidden" image="https://picsum.photos/id/99/300/300" title="Neruda" subTitle="Pablo Neskua" />
+        </div>
+    </div>
+</template>
---
+++ src/views/SearchView.vue
@@ -0,0 +1,59 @@
+<script setup>
+    import CategorySelect from '../components/CategorySelect.vue';
+</script>
+
+<template>
+    <div class="p-8">
+        <button
+            type="button"
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+            Browse all
+        </button>
+
+        <div class="py-1.5"></div>
+
+        <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
+            <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300"/>
+            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/45/300/300"/>
+            <CategorySelect category="Made For You" image="https://picsum.photos/id/76/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/56/300/300"/>
+            <CategorySelect category="Hot 10" image="https://picsum.photos/id/25/300/300"/>
+            <CategorySelect category="Rock Music" image="https://picsum.photos/id/103/300/300"/>
+            <CategorySelect category="Top 100" image="https://picsum.photos/id/244/300/300"/>
+            <CategorySelect category="Pop Music" image="https://picsum.photos/id/202/300/300"/>
+            <CategorySelect category="Popular Music" image="https://picsum.photos/id/101/300/300"/>
+            <CategorySelect category="Hiphop Muic" image="https://picsum.photos/id/120/300/300"/>
+            <CategorySelect category="Jazz and Blues" image="https://picsum.photos/id/40/300/300"/>
+            <CategorySelect category="Rhythm Music" image="https://picsum.photos/id/45/300/300"/>
+            <CategorySelect category="Classical Music" image="https://picsum.photos/id/76/300/300"/>
+            <CategorySelect category="Country Muic" image="https://picsum.photos/id/56/300/300"/>
+            <CategorySelect category="World Music" image="https://picsum.photos/id/25/300/300"/>
+            <CategorySelect category="Heavy Metal" image="https://picsum.photos/id/103/300/300"/>
+            <CategorySelect category="Folk Music" image="https://picsum.photos/id/244/300/300"/>
+            <CategorySelect category="Funk Music" image="https://picsum.photos/id/202/300/300"/>
+            <CategorySelect category="Disco" image="https://picsum.photos/id/101/300/300"/>
+            <CategorySelect category="Latin" image="https://picsum.photos/id/120/300/300"/>
+            <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300"/>
+            <CategorySelect category="Salsa Music" image="https://picsum.photos/id/45/300/300"/>
+            <CategorySelect category="Opera Music" image="https://picsum.photos/id/76/300/300"/>
+            <CategorySelect category="Music of Africa" image="https://picsum.photos/id/56/300/300"/>
+            <CategorySelect category="Music of India" image="https://picsum.photos/id/25/300/300"/>
+            <CategorySelect category="New Wave" image="https://picsum.photos/id/103/300/300"/>
+            <CategorySelect category="Acoustic Music" image="https://picsum.photos/id/244/300/300"/>
+            <CategorySelect category="Made For You" image="https://picsum.photos/id/202/300/300"/>
+            <CategorySelect category="College Rock" image="https://picsum.photos/id/101/300/300"/>
+            <CategorySelect category="Soul Music" image="https://picsum.photos/id/120/300/300"/>
+            <CategorySelect category="Classical Blues" image="https://picsum.photos/id/40/300/300"/>
+            <CategorySelect category="Hindi Songs" image="https://picsum.photos/id/45/300/300"/>
+            <CategorySelect category="Kannada Songs" image="https://picsum.photos/id/76/300/300"/>
+            <CategorySelect category="Electronic Dance" image="https://picsum.photos/id/56/300/300"/>
+            <CategorySelect category="Polka Music" image="https://picsum.photos/id/25/300/300"/>
+            <CategorySelect category="Signature Songs" image="https://picsum.photos/id/103/300/300"/>
+            <CategorySelect category="To Remember" image="https://picsum.photos/id/244/300/300"/>
+            <CategorySelect category="Forever Classic" image="https://picsum.photos/id/202/300/300"/>
+            <CategorySelect category="Punk Rock" image="https://picsum.photos/id/101/300/300"/>
+            <CategorySelect category="Indie Rock" image="https://picsum.photos/id/120/300/300"/>
+        </div>
+    </div>
+</template>
